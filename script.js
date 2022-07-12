75167a146fe70366ff8ab966fe5d8f08c464b4bb
unknown <klammer.lukas@hotmail.com>
2022-07-12 15:34:57 +0200

    fetch data from server

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 
 
-function checkZip() {
+async function checkZip() {
     const zip = document.getElementById('zip');
+    let timestamp = + new Date();
     if (zip.value.length == 5) {
         console.log('Die zu suchende zip ist: ', zip.value);
-        const interface = 'https://www.postdirekt.de/plzserver/PlzAjaxServlet';
+        try {
+            const url = `https://cors-anywhere.herokuapp.com/https://www.postdirekt.de/plzserver/PlzAjaxServlet?finda=city&city=${zip.value}&lang=de_DE`;
+            const response = await fetch(url);
+            console.log(response);
+        } catch (e) {
+            alert('Fehler beim Laden: ', e);
+        }
+        setCityInput('Musterstadt');
     }
 }
 
-function searchCityByZip() {
 
+function setCityInput(text) {
+    document.getElementById('city').value = text;
 }
 
 
@@ -17,3 +26,23 @@ function searchCityByZip() {
 function getJson() {
 
 }
+
+
+
+// Wenn es mit formData gemacht werden muss
+
+        // let formData = new FormData();
+        // formData.append('finda', 'city');
+        // formData.append('city', zip.value);
+        // formData.append('lang', 'de_DE')
+
+        // const url = `https://www.postdirekt.de/plzserver/PlzAjaxServlet?nocache=${timestamp}`;
+        // const fetchOptions = {
+        //     method: 'POST',
+        //     body: formData,
+        // };
+
+        // if (!response.ok) {
+        //     const errorMessage = await response.text();
+        //     throw new Error(errorMessage);
+        // }
