b0bf57a9775cefc17c976e1b2b67cdca68dadf8a
unknown <klammer.lukas@hotmail.com>
2022-07-12 12:11:30 +0200

    check, if zip has right length

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
 
+function checkZip() {
+    const zip = document.getElementById('zip');
+    if (zip.value.length == 5) {
+        console.log('Die zu suchende zip ist: ', zip.value);
+        const interface = 'https://www.postdirekt.de/plzserver/PlzAjaxServlet';
+    }
+}
+
 function searchCityByZip() {
 
 }
