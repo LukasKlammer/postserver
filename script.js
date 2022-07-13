0c5fed5c7fc922a0ac0ad85c8c43f6a654d67638
unknown <klammer.lukas@hotmail.com>
2022-07-13 09:24:27 +0200

    fetch street data by zip, select input for citys, read all citys fro a zip

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+let citys = [];
+let streets = ['Teststraße', 'Müllergasse'];
 
-
-
-async function handOverCityName() {
+async function handOverCityAndStreets() {
     const zip = document.getElementById('zip').value;
     if (zip.length == 5) {
-        const responseAsJson = await loadByZip(zip);
-        setCityInput(responseAsJson, zip);
-    } 
+        const foundCityData = await loadByZip(zip);
+        findAllCitys(foundCityData);
+        setCitySelectInput(zip);
+        if (foundCityData['rows']) {
+            const city = foundCityData['rows'][0]['city'];
+            // const foundStreetData = await loadByZipAndCity(zip, city);
+            // console.log(foundStreetData);
+        }
+
+    }
 }
 
 
@@ -28,16 +35,53 @@ async function loadByZip(zip) {
 }
 
 
-function setCityInput(responseAsJson, zip) {
-    console.log(responseAsJson);
-    if (responseAsJson['rows']) {
-        const city = responseAsJson['rows'][0]['city'];
-        document.getElementById('city').value = city;
+function findAllCitys(foundCityData) {
+    citys = [];
+    const cityInfos = foundCityData['rows'];
+    if (cityInfos) {
+        for (let i = 0; i < cityInfos.length; i++) {
+            const city = cityInfos[i]['city'];
+            if (!citys.includes(city)) {
+                citys.push(city);
+            }
+        }
+    }
+}
+
+
+function setCitySelectInput(zip) { // Hinweis: Kann auch mehrere Ortsteile geben, z. B. 91126 hat 3 Ortsteile
+    if (citys.length >= 0) {
         document.getElementById('not-found').classList.add('d-none');
+        let selectInput = document.getElementById('citys');
+        selectInput.innerHTML = '';
+        for (let i = 0; i < citys.length; i++) {
+            const city = citys[i];
+            opt = document.createElement("option");
+            opt.value = city;
+            opt.textContent = city;
+            selectInput.appendChild(opt);
+        }
     } else {
         document.getElementById('not-found').classList.remove('d-none');
         document.getElementById('not-found').innerHTML = `0 Suchtreffer für "${zip}"`;
     }
+}
+
+async function loadByZipAndCity(zip, city) {
+    let district = '';
+    try {
+        const url = `https://cors-anywhere.herokuapp.com/https://www.postdirekt.de/plzserver/PlzAjaxServlet?finda=streets&plz_plz=${zip}&plz_city=${city}&plz_district=${district}=&lang=de_DE`;
+        const response = await fetch(url);
+        const responseAsJson = await response.json();
+        return responseAsJson;
+    } catch (e) {
+        alert('Fehler beim Laden: ', e);
+    }
+}
+
+
+function setStreetsInput() {
+
 }
 
 
